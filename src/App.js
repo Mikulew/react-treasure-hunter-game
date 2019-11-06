eda00cf69f68de1ab0784ae6e7649c1a5d810dab
Mikulew <mikulew@gmail.com>
2019-11-06 18:08:26 +0100

    Adding GameHeader component and Game.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GameHeader from './components/GameHeader';
 import './css/App.css';
+import './css/Game.css';
 
 function App() {
   return (
     <div className="App">
-      <main className="App-header">Treasure Hunter Game</main>
+      <GameHeader />
     </div>
   );
 }
---
+++ src/components/GameHeader.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GameHeader = () => <h1 className="game-title">Treasure Hunter Game</h1>;
+
+export default GameHeader;
